f1de441eae1c7724790da2b7a494fa60143077d1
lNalator <enzomidonetpro@gmail.com>
2024-10-03 14:42:56 +0200

    EM : added swagger open api

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from './modules/app.module';
+import { config } from './swaggerConfig';
+import { SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
---
+++ src/swaggerConfig.ts
@@ -0,0 +1,10 @@
+import { DocumentBuilder } from '@nestjs/swagger';
+
+export const config = new DocumentBuilder()
+  .setTitle('Ynote API')
+  .setDescription(
+    'Here lies the API of Ynote, needed for the weel-being of the Ynote app.',
+  )
+  .setVersion('0.1')
+  .addTag('Ynote')
+  .build();
