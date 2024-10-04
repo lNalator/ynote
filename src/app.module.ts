8469975c769a5b2827b9d1c944444eeba6a70bf2
lNalator <enzomidonetpro@gmail.com>
2024-10-04 09:44:44 +0200

    EM : Added swagger and matiere table

---
+++ src/app.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './controllers/app.controller';
+import { AppService } from './app.service';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { Matiere } from './tables/matiere/matiere.model';
+
+@Module({
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'test',
+      models: [Matiere],
+      // autoLoadModels: true,
+      // synchronize: true,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@nestjs/common';
+import { Sequelize } from 'sequelize';
 
 @Injectable()
 export class AppService {
