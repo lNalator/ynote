d5a35a66df4d08231c316423f1fb0a955ea4d1a4
MaximeOriot <maxime9278@gmail.com>
2024-11-11 18:08:07 +0100

    helmet configuration

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { config } from './config/swagger.config';
 import { SwaggerModule } from '@nestjs/swagger';
 import { GlobalExceptionsFilter } from './middleware/globalExceptionsFilter';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +13,36 @@ async function bootstrap() {
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
   });
+
+  app.use(
+    helmet({
+      contentSecurityPolicy: {
+        directives: {
+          defaultSrc: ["'self'", "'unsafe-inline'", "'unsafe-eval'"],
+          scriptSrc: ["'self'", "'unsafe-inline'", "'unsafe-eval'"],
+          styleSrc: ["'self'", "'unsafe-inline'"],
+          fontSrc: ["'self'", "'unsafe-inline'"],
+          imgSrc: ["'self'", "'unsafe-inline'"],
+          connectSrc: ["'self'", "'unsafe-inline'", "ws://*.ynote.com", "wss://*.ynote.com"],
+          frameSrc: ["'self'", "'unsafe-inline'"],
+          workerSrc: ["'self'", "'unsafe-inline'"],
+          objectSrc: ["'self'", "'unsafe-inline'"],
+          mediaSrc: ["'self'", "'unsafe-inline'"],
+          childSrc: ["'self'", "'unsafe-inline'"],
+          upgradeInsecureRequests: [],
+        },
+      },
+      hidePoweredBy: true,
+      xssFilter: true,
+      hsts: {
+        maxAge: 31536000,
+        includeSubDomains: true,
+        preload: true,
+      },
+      referrerPolicy: { policy: 'strict-origin-when-cross-origin' },
+      crossOriginEmbedderPolicy: { policy: 'credentialless' },
+    })
+  );
 
   app.useGlobalFilters(new GlobalExceptionsFilter());
 
